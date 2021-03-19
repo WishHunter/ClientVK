9c20845d5d4822282f7578116b849a4d5e163ef0
d.molkov <d.molkov@fbs.com>
2021-03-19 13:15:55 +0300

    true version

--- ClientVK/Controllers/NewsTable.swift
+++ ClientVK/Controllers/NewsTable.swift
@@ -12,24 +12,18 @@ class NewsTable: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(UINib(nibName: "NewsCell", bundle: nil), forCellReuseIdentifier: "NewsCell")
-    }
+        tableView.register(UINib(nibName: "HelloCell", bundle: nil), forCellReuseIdentifier: "HelloCell")
+=    }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsCell
-        
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HelloCell", for: indexPath) as! HelloCell
+=
         return cell
     }
 }
--- ClientVK/Views/cells/NewsCell.swift
+++ ClientVK/Views/cells/NewsCell.swift
@@ -10,11 +10,4 @@ import UIKit
 class NewsCell: UITableViewCell {
     
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
 }
