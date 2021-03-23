f330f73cb1fac0e745bef0562c5d73bd64b0bd12
WishHunter <molkovd@ya.ru>
2021-03-24 00:26:07 +0300

    add image

--- ClientVK/Controllers/PhotoToFriendCollectionViewController.swift
+++ ClientVK/Controllers/PhotoToFriendCollectionViewController.swift
@@ -7,15 +7,19 @@
 
 import UIKit
 
-class PhotoToFriendCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class PhotoToFriendCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
+    @IBOutlet weak var collectionView: UICollectionView!
     var user: User?
     
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        
+        
+        
     }
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
@@ -30,11 +34,11 @@ class PhotoToFriendCollectionViewController: UICollectionViewController, UIColle
         return CGSize(width: width, height: width)
     }
     
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return user?.photos.count ?? 0
     }
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoToFriendCollectionViewCell
 
         if let image = user?.photos[indexPath.item][0] {
@@ -47,17 +51,58 @@ class PhotoToFriendCollectionViewController: UICollectionViewController, UIColle
         return cell
     }
     
-    //MARK: - Segue
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard segue.identifier == "selectedPhoto" else {
-            return
-        }        
-        let photoController = segue.destination as! FullScreenPhotoViewController
-        
-        if let indexPath = self.collectionView.indexPathsForSelectedItems,
-           let image = user?.photos[indexPath[0][1]][0]{
-            photoController.photoName = String(image)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let image = user?.photos[indexPath.item][0] {
+            createFullScreenPhoto(image)
         }
+    }
+}
+
+
+extension PhotoToFriendCollectionViewController {
+    
+    enum PositionImage {
+        case center, left, right
+    }
+    func createFullScreenPhoto(_ image: String) {
+        let view = UIView()
+        addImage(image: image, position: .center)
+        
+        self.view.addSubview(view)
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
+        leftSwipe.direction = .left
+        view.addGestureRecognizer(leftSwipe)
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
+        rightSwipe.direction = .right
+        view.addGestureRecognizer(rightSwipe)
+        
+    }
+    
+    func addImage(image: String, position: PositionImage) {
+        let imageView = UIImageView()
+        
+        view.frame = self.view.bounds
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        
+        imageView.image = UIImage(named: image)
+        imageView.frame = view.bounds
+        imageView.transform = CGAffineTransform(translationX: 0, y: 0)
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        
+        view.addSubview(imageView)
+    }
+    
+    @objc func onSwipe(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+                case .left:
+                    print("Left Swipe")
+                case .right:
+                    print("Right Swipe")
+                default:
+                    break
+                }
     }
 }
