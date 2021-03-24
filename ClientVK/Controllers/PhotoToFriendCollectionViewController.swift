d65a2279f7ec665fe941fa79885fc6a6bda18888
d.molkov <d.molkov@fbs.com>
2021-03-24 12:13:50 +0300

    add slider

--- ClientVK/Controllers/PhotoToFriendCollectionViewController.swift
+++ ClientVK/Controllers/PhotoToFriendCollectionViewController.swift
@@ -11,6 +11,11 @@ class PhotoToFriendCollectionViewController: UIViewController, UICollectionViewD
     
     @IBOutlet weak var collectionView: UICollectionView!
     var user: User?
+    var viewFullScreen: UIView?
+    var previewImage: UIImageView?
+    var activeImage: UIImageView?
+    var nextImage: UIImageView?
+    var activeImageIndex: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,8 +57,8 @@ class PhotoToFriendCollectionViewController: UIViewController, UICollectionViewD
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let image = user?.photos[indexPath.item][0] {
-            createFullScreenPhoto(image)
+        if let _ = user?.photos[indexPath.item][0] {
+            createFullScreenPhoto(indexPath.item)
         }
     }
 }
@@ -64,11 +69,22 @@ extension PhotoToFriendCollectionViewController {
     enum PositionImage {
         case center, left, right
     }
-    func createFullScreenPhoto(_ image: String) {
-        let view = UIView()
-        addImage(image: image, position: .center)
+    
+    func createFullScreenPhoto(_ index: Int) {
+        activeImageIndex = index
+        viewFullScreen = UIView()
+        viewFullScreen!.frame = self.view.bounds
+        viewFullScreen!.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        self.view.addSubview(viewFullScreen!)
         
-        self.view.addSubview(view)
+        previewImage = UIImageView()
+        activeImage = UIImageView()
+        nextImage = UIImageView()
+        
+        createImages()
+        viewFullScreen!.addSubview(previewImage!)
+        viewFullScreen!.addSubview(activeImage!)
+        viewFullScreen!.addSubview(nextImage!)
         
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
         leftSwipe.direction = .left
@@ -80,29 +96,72 @@ extension PhotoToFriendCollectionViewController {
         
     }
     
-    func addImage(image: String, position: PositionImage) {
-        let imageView = UIImageView()
-        
-        view.frame = self.view.bounds
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-        
+    func createImages() {
+        if (activeImageIndex! - 1) >= 0, let image = user?.photos[activeImageIndex! - 1][0] {
+            addImage(imageView: previewImage!, image: image, position: .left)
+        }
+        if let image = user?.photos[activeImageIndex!][0] {
+            addImage(imageView: activeImage!, image: image, position: .center)
+        }
+        if (activeImageIndex! + 1) < (user?.photos.count)!, let image = user?.photos[activeImageIndex! + 1][0] {
+            addImage(imageView: nextImage!, image: image, position: .right)
+        }
+    }
+    
+    func addImage(imageView: UIImageView, image: String, position: PositionImage) {
+               
         imageView.image = UIImage(named: image)
         imageView.frame = view.bounds
-        imageView.transform = CGAffineTransform(translationX: 0, y: 0)
         imageView.contentMode = .scaleAspectFit
         imageView.isUserInteractionEnabled = true
         
-        view.addSubview(imageView)
+        switch position {
+            case .left:
+                imageView.frame.origin.x = -self.view.bounds.width
+            case .center:
+                imageView.frame.origin.x = (self.view.bounds.width / 2) - (imageView.frame.width / 2)
+            case .right:
+                imageView.frame.origin.x = self.view.bounds.width
+            }
     }
     
     @objc func onSwipe(_ sender: UISwipeGestureRecognizer) {
-        switch sender.direction {
+                
+        let animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) {
+            switch sender.direction {
                 case .left:
-                    print("Left Swipe")
+                    if (self.activeImageIndex! + 1) < (self.user?.photos.count)! {
+                        self.activeImage?.frame.origin.x = -self.view.bounds.width
+                        self.nextImage?.frame.origin.x = (self.view.bounds.width / 2) - ((self.nextImage?.frame.width)! / 2)
+                    }
                 case .right:
-                    print("Right Swipe")
+                    if (self.activeImageIndex! - 1) >= 0 {
+                        self.activeImage?.frame.origin.x = self.view.bounds.width
+                        self.previewImage?.frame.origin.x = (self.view.bounds.width / 2) - ((self.nextImage?.frame.width)! / 2)
+                    }
                 default:
                     break
                 }
+        }
+        
+        animator.addCompletion{position in
+            if position == .end {
+                switch sender.direction {
+                    case .left:
+                        if (self.activeImageIndex! + 1) < (self.user?.photos.count)! {
+                            self.activeImageIndex! += 1
+                        }
+                    case .right:
+                        if (self.activeImageIndex! - 1) >= 0 {
+                            self.activeImageIndex! -= 1
+                        }
+                    default:
+                        break
+                    }
+                self.createImages()
+            }
+        }
+        
+        animator.startAnimation()
     }
 }
