0e7fc0528bd7fab6a2c74c3563664181070da3e0
WishHunter <molkovd@ya.ru>
2021-04-10 16:38:49 +0300

    add singleton Session

---
+++ ClientVK/Models/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  ClientVK
+//
+//  Created by Denis Molkov on 10.04.2021.
+//
+
+import Foundation
+
+final
+class Session {
+    
+    static let instance = Session()
+    private init() {}
+    
+    var token: String? = nil
+    var userId: Int? = nil
+}
