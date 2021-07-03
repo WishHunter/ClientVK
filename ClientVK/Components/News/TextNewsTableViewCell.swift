9877c4571218fa2be9a9cc195f3e2615331e07d2
Denis <MolkovD@ya.ru>
2021-07-03 18:50:16 +0300

    nothing

--- ClientVK/Components/News/TextNewsTableViewCell.swift
+++ ClientVK/Components/News/TextNewsTableViewCell.swift
@@ -15,45 +15,36 @@ class TextNewsTableViewCell: UITableViewCell {
     var fullText: String?
     var shortText: String? {
         didSet {
-            createShowMoreLink()
-//            self.textNews.text = shortText
-//            if shortText != fullText {
-//                createShowMoreLink()
-//            }
+            self.textNews.text = shortText
+            if shortText != fullText {
+                createShowMoreLink()
+            }
         }
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-//        createShowMoreLink()
     }
     
     func createShowMoreLink() {
         let button = UIButton(type: .system)
-        
-//        self.addSubview(button)
+        button.setTitle("Show more", for: .normal)
+        button.setTitleColor(.lightGray, for: .selected)
+        button.addTarget(self, action: #selector(showMore), for: .touchDown)
         container.addSubview(button)
         
-        button.setTitle("Show more", for: .normal)
-        button.setTitle("Less more", for: .selected)
-        self.textNews.text = shortText
-
-        textNews.translatesAutoresizingMaskIntoConstraints = false
         button.translatesAutoresizingMaskIntoConstraints = false
-        container.translatesAutoresizingMaskIntoConstraints = false
+        textNews.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            container.topAnchor.constraint(greaterThanOrEqualTo: self.topAnchor, constant: 10),
-            container.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor, constant: 10),
-            container.rightAnchor.constraint(greaterThanOrEqualTo: self.rightAnchor, constant: -10),
-            container.bottomAnchor.constraint(greaterThanOrEqualTo: self.bottomAnchor, constant: -10),
-            textNews.topAnchor.constraint(greaterThanOrEqualTo: container.topAnchor, constant: 0),
-            textNews.leftAnchor.constraint(greaterThanOrEqualTo: container.leftAnchor, constant: 0),
-            textNews.rightAnchor.constraint(greaterThanOrEqualTo: container.rightAnchor, constant: 0),
-            textNews.bottomAnchor.constraint(greaterThanOrEqualTo: button.topAnchor, constant: -10),
-            button.leftAnchor.constraint(greaterThanOrEqualTo: container.leftAnchor, constant: 0),
-            button.bottomAnchor.constraint(greaterThanOrEqualTo: container.bottomAnchor, constant: 0)
+            textNews.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -20),
+            button.leftAnchor.constraint(equalTo: container.leftAnchor, constant: 0),
+            button.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -20)
         ])
         
         print(self.bounds.height)
+    }
+    
+    @objc private func showMore(_ sender: UIButton) {
+        print(fullText!)
     }
 }
